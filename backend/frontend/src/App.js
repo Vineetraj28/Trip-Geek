eeabecc84a482331d1485cb43f87880b02115fef
Vineetraj28 <vineetkojha@gmail.com>
2022-07-15 14:18:19 +0530

    my first commit

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,7 +1,7 @@
 import "./app.css";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { useEffect, useState } from "react";
-import { Room, Star, StarBorder } from "@material-ui/icons";
+import { Room, Star,  } from "@material-ui/icons";
 import axios from "axios";
 import { format } from "timeago.js";
 import Register from "./components/Register";
